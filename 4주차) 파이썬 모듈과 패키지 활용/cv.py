6ddbfc4f8f3e7452023f4f4caa59bbe17a3f7539
wanikim1217 <wanikim1217@gmail.com>
2023-03-21 20:31:08 +0900

    4주차) 파이썬 모듈과 패키지 활용

--- 4주차) 파이썬 모듈과 패키지 활용/cv.py
+++ 4주차) 파이썬 모듈과 패키지 활용/cv.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 
-imageFile = './sjcu_python-programming/data/lena.jpg'
+imageFile = './data/lena.jpg'
 img = cv2.imread(imageFile)
 cv2.imshow('Lena color', img)
 
-cv2.imwrite('./sjcu_python-programming/data/Lena.png', img)
+cv2.imwrite('./data/Lena.png', img) # 위에서 읽은 lena.png 파일을 Lena.png로 새롭게 저장
 
-imageFile = './data/Lena.png'
-img = cv2.imread(imageFile, cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Lena gray', img)
+imageFile = './data/Lena.png' # 위에서 새로 저장한 Lena.png파일을 변수에 넣어 불러옴
+img = cv2.imread(imageFile, cv2.IMREAD_GRAYSCALE) # imread함수를 사용하여 imageFile 변수에 새롭게 넣은 Lena.png파일을 Grayscale 색상으로 읽어옴
+cv2.imshow('Lena gray', img) # Grayscale 색상으로 가지고 온 Lena.png파일을 띄어달라는 명령어
 
-cv2.waitKey()
-cv2.destroyAllWindows()
+cv2.waitKey() # 사용자가 어떤 키든 입력하기만을 기다려라
+cv2.destroyAllWindows() # 어떤 키든 입력하기만 하면 모든 윈도우를 닫아라
